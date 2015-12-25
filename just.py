4d3ddae41080e51e272b01c075144ee78eafc168
manojprithvee <manoj.prithvee@gmail.com>
2015-12-25 16:42:45 +0530

    added wait for internet in just and corrected exception handling

--- just.py
+++ just.py
@@ -1,4 +1,16 @@
 import requests,lxml.html as lh,re,os,subprocess,json
+def wait_for_internet():
+    print ('Waiting for internet..')
+    while True:
+        p = subprocess.Popen("ping -c 1 8.8.8.8", stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=True,
+                             executable="/bin/bash")
+        output = p.stdout.read()
+        if output.find("100% packet loss") == -1 and output.find("connect: Network is unreachable") == -1:
+            return
+        else:
+            sys.stdout.write("..")
+            sys.stdout.flush()
+            time.sleep(2)
 def notify(title1,message,try1=1,pri=0):
 	if try1>=4:
 		print "4 Retries failed"
@@ -7,19 +19,13 @@ def notify(title1,message,try1=1,pri=0):
 		a=requests.post('https://api.parse.com/1/push', data=json.dumps({"where": {"deviceType": "android"},"data": {"alert": message,"title":title1,"flag":""}}), headers={"X-Parse-Application-Id": "fMB6piQyYMpDbCnkJFrlfPZVS5nihQfADGqycvTH","X-Parse-REST-API-Key": "jiBr1uM5ip7oSYzwNYlL9QzI6eM62xfKxR3y5u3b","Content-Type": "application/json"})
 	except Exception, e:
 		print str(e)
+		wait_for_internet()
+
 		notify(title1,message,try1+1)
 class justdubbed(object):
 	
 	def __init__(self, link):
 		super(justdubbed, self).__init__()
-		pattern = re.compile("http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+")
-    	a = raw_input("enter the Link:")
-    	if not pattern.match(a):
-    		print "enter a correct link"
-    		os._exit()
-    	if link.find("www.justanimedubbed.tv/")==-1:
-    		print "enter a correct link"
-    		os._exit()
 		self.s_name=link.split("/")[-2]
 		notify("JustDubbedAnime Downloader","Started Downloading "+self.s_name)
 		self.justdubbedlevel1(link)
@@ -32,6 +38,7 @@ class justdubbed(object):
 			raw_data=requests.get(link)
 		except Exception, e:
 			print str(e)
+			wait_for_internet()
 			justdubbedlevel1(link,try1+1)
 			return
 		doc = lh.fromstring(raw_data.text)
@@ -51,6 +58,7 @@ class justdubbed(object):
 			raw_data=requests.get(Episold_Link)
 		except Exception, e:
 			print str(e)
+			wait_for_internet()
 			self.justdubbedlevel2(Episold_Link,Episold_Name,try1+1)
 			return
 		doc = lh.fromstring(raw_data.text)
@@ -62,10 +70,15 @@ class justdubbed(object):
 		Embeded_Links=temp
 		self.justdubbedlevel3(Embeded_Links[0],Episold_Name)
 
-	def justdubbedlevel3(self,Embeded_Link,Episold_Name):
+	def justdubbedlevel3(self,Embeded_Link,Episold_Name,try1=1):
 		print Episold_Name +"\n\n"
-		a = requests.get(Embeded_Link)
-		urls = re.findall('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', a.text)
+		try:
+			a = requests.get(Embeded_Link)
+			urls = re.findall('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', a.text)
+		except Exception,e:
+			print str(e)
+			wait_for_internet()
+			justdubbedlevel3(self,Embeded_Link,Episold_Name,try1+1)
 		for i in urls:
 			if i.find(".mp4") != -1:
 				urls = i[:-2]
@@ -101,7 +114,6 @@ def Run_process(exe,Episold_Name):
                     	pass
         final = final + out
         if retcode is not None:
-            return final
-
-justdubbed(raw_input("Enter the link: "))
+            return final	
+justdubbed("http://www.justanimedubbed.tv/watch/rurouni-kenshin/")
 
